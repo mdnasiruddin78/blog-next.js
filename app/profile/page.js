b5cc000cd5744e945749261e48ec72a70d08d7ca
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-24 17:50:16 +0600

    login logout form add

--- app/profile/page.js
+++ app/profile/page.js
@@ -18,21 +18,3 @@ const Profile =async () => {
 };
 
 export default Profile;
-
-
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { redirect } from "next/navigation";
-
-// const Profile = async () => {
-
-//     const { isAuthenticated } = getKindeServerSession();
-//     const isUserAuthenticated = await isAuthenticated();
-
-//     if (!isUserAuthenticated){
-//         redirect("/api/auth/login");
-//     }
-
-//     return (
-//         <div className="p-5">
-//             <div className="p-5 bg-gray-300">
-//                 <div className="p-5 bg-gray-100">
